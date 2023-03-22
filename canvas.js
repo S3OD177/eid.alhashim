ea2318753d1852b5959dc9cb5107299a8eeefc8c
Saud Alholaibi <saudsa517@gmail.com>
2023-03-22 14:59:48 +0300

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -34,7 +34,7 @@ function setImage() {
         context.textAlign= 'center';
      
         context.moveTo(0,canvas.height/2);
-        context.fillText(document.getElementById("txtName").value, 500, 900);
+        context.fillText(document.getElementById("txtName").value, 600, 800);
  
         var dataURL = canvas.toDataURL("image/jpg", 1.0);
         var a = document.createElement("a");
